56a76c8015a4f8b97dfaff39e364c5ac9cee1f28
Kyle <Kyle>
2015-05-08 00:18:46 -0500

    added project

---
+++ libGDX/desktop/src/com/kornetzke/Test/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kornetzke.Test.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kornetzke.Test.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
